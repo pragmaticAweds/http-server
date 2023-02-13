a2ddcd3c52d333bd3a0eba437ea58b2715c5a064
AbdulAfeez Jimoh <jimohafeez738@gmail.com>
2023-02-13 01:14:15 +0100

    completed worker thread

--- worker-threads-example/thread.js
+++ worker-threads-example/thread.js
@@ -1,8 +1,13 @@
-const { Worker, isMainThread } = require("worker_threads");
+const { Worker, isMainThread, workerData } = require("worker_threads");
 
 if (isMainThread) {
-  new Worker(__filename);
-  new Worker(__filename);
+  new Worker(__filename, {
+    workerData: [9, 0, 3, 4, 2, 6, 1],
+  });
+  new Worker(__filename, {
+    workerData: [3, 4, 2, 6, 5, 9, 0],
+  });
 } else {
-  console.log();
+  console.log("Worker in progress on process: " + process.pid);
+  console.log(workerData + " is sorted as " + workerData.sort());
 }
